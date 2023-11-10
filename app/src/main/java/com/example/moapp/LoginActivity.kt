7cc69cfc4bbbb410f6351eb082ed0c82c787cf4f
Jonghyeok <jhpmuscle@gmail.com>
2023-11-10 16:59:43 +0900

    [feat] gradle 수정, 로그인 페이지 레이아웃

---
+++ app/src/main/java/com/example/moapp/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.moapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding =
+        setContentView(R.layout.activity_login)
+    }
+}
