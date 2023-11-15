7d679c33b2923a46bf04d270360dc7c2d2a4eafe
goldanghenry <wshdhkd@naver.com>
2023-11-15 14:50:47 +0900

    Group chat list

---
+++ app/src/main/java/com/example/moapp/model/ChatModel.kt
@@ -0,0 +1,11 @@
+package com.example.moapp.model
+
+data class User(val id: String, val name: String, val img: String)
+
+data class Comment(val message: String)
+
+data class ChatModel(
+    val users: List<User>,
+    val roomId: String, // 추가: 채팅방 식별자
+    val comments: Map<String, Comment>
+)
