4d7885c44325eb8dd79972bbebb797daceafdd51
Jonghyeok <jhpmuscle@gmail.com>
2023-11-15 22:11:18 +0900

    [feat] Kakao Login, Retrofit 세팅, 로그인 UI 수정

--- app/src/main/java/com/example/moapp/MainActivity.kt
+++ app/src/main/java/com/example/moapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.moapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.kakao.sdk.common.util.Utility
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("park", "KeyHash: ${Utility.getKeyHash(this)}")
     }
 }
