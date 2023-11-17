34d67da907ce659f6c609fd1d005ad037bb7b445
Jonghyeok <jhpmuscle@gmail.com>
2023-11-17 12:17:28 +0900

    [feat] Login, Token전역 저장, Retrofit API Setting

--- app/src/main/java/com/example/moapp/MainActivity.kt
+++ app/src/main/java/com/example/moapp/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d("park", "KeyHash: ${Utility.getKeyHash(this)}")
+        val token = PrefApp.prefs.getString("accessToken", "defalut")
+        Log.d("park", token)
     }
 }
